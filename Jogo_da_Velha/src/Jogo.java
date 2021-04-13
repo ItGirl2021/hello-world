76e46acc62b93f72c08cd7664bea90f1c3a90571
ItGirl2021 <guia.danielle@gmail.com>
2021-04-13 09:31:28 -0300

    New versions

--- Jogo_da_Velha/src/Jogo.java
+++ Jogo_da_Velha/src/Jogo.java
@@ -4,15 +4,15 @@ import java.awt.event.*;
 import javax.swing.border.EmptyBorder;
 
 public class Jogo extends JFrame {
-    /**
-     *
-     */
+
     private static final long serialVersionUID = 1L;
-    JButton cor; //Para restaurar a cor dos botões;
+
     JButton [] botoes; //Vetor para Buttons;
     JPanel PainelJogo; //Painel para os botões;
     int vezJogador = 0; //Verificar a vez do jogador;
     String simbolo; //Para alocar o simbolo x ou o;
+    int jogador1 = 0; //Pontuação Jogador 1;
+    int jogador2 = 0; //Pontuação jogador 2;
 
     public Jogo() {
         super("Jogo da Velha"); //Título da Janela;
@@ -20,94 +20,122 @@ public class Jogo extends JFrame {
 
         PainelJogo = new JPanel(); //Cria novo painel;
         PainelJogo.setLayout(new GridLayout(3,3,10,10)); //Atribui Layout do tipo GridLayout;
-        cor = new JButton();
-
+        PainelJogo.setBackground(Color.BLACK);
         botoes = new JButton[9];
 
         for(int i = 0; i < 9; i++) {
             botoes[i] = new JButton("");
+            botoes[i].setFont(new Font("Calibri", Font.BOLD, 70));
+            PainelJogo.add(botoes[i]);
         }
 
-        botoes[0].setFont(new Font("Calibri", Font.BOLD, 70));
         botoes[0].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(0);
+                                
+                if(Verifica(0)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[0]);
 
-        botoes[1].setFont(new Font("Calibri", Font.BOLD, 70));
         botoes[1].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(1);
+                                                
+                if(Verifica(1)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[1]);
-
-        botoes[2].setFont(new Font("Calibri", Font.BOLD, 70));
+       
         botoes[2].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(2);
+                                                
+                if(Verifica(2)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[2]);
-
-        botoes[3].setFont(new Font("Calibri", Font.BOLD, 70));
+        
         botoes[3].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(3);
+                                                
+                if(Verifica(3)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[3]);
-
-        botoes[4].setFont(new Font("Calibri", Font.BOLD, 70));
+        
         botoes[4].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(4);
+                
+                if(Verifica(4)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[4]);
-
-        botoes[5].setFont(new Font("Calibri", Font.BOLD, 70));
+        
         botoes[5].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(5);
+                                                
+                if(Verifica(5)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[5]);
-
-        botoes[6].setFont(new Font("Calibri", Font.BOLD, 70));
+        
         botoes[6].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(6);
+                                                
+                if(Verifica(6)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[6]);
-
-        botoes[7].setFont(new Font("Calibri", Font.BOLD, 70));
+        
         botoes[7].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(7);
+                                                
+                if(Verifica(7)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[7]);
-
-        botoes[8].setFont(new Font("Calibri", Font.BOLD, 70));
+        
         botoes[8].addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Jogada(8);
+                                                
+                if(Verifica(8)) {
+                    JOptionPane.showMessageDialog(null, "You Win!");
+                    Reset();
+                }
             }
         });
-        PainelJogo.add(botoes[8]);
 
         add(PainelJogo); //Adiciona painel a janela;
         setLocation(470, 200); //Determina a localização da janela;
@@ -116,6 +144,7 @@ public class Jogo extends JFrame {
     }
 
     public void Jogada(int simbol) {
+
         if(vezJogador == 0) {
             simbolo = "X";
             vezJogador = 1;
@@ -126,5 +155,125 @@ public class Jogo extends JFrame {
 
         botoes[simbol].setText(simbolo);
         botoes[simbol].setEnabled(false); //Desativa o botão;
+    }
+
+    public boolean Verifica(int simbol) {
+        switch(simbol) {
+            case 0:
+                if(((botoes[0].getText()).equals((botoes[1].getText())) && (botoes[0].getText()).equals((botoes[2].getText())))
+                    || ((botoes[0].getText()).equals((botoes[3].getText())) && (botoes[0].getText()).equals((botoes[6].getText())))
+                    || ((botoes[0].getText()).equals((botoes[4].getText())) && (botoes[0].getText()).equals((botoes[8].getText())))) {
+                    
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            case 1:
+                if(((botoes[1].getText()).equals((botoes[0].getText())) && (botoes[1].getText()).equals((botoes[2].getText())))
+                    || ((botoes[1].getText()).equals((botoes[4].getText())) && (botoes[1].getText()).equals((botoes[7].getText())))) {
+                    
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+
+            case 2:
+                if(((botoes[2].getText()).equals((botoes[1].getText())) && (botoes[2].getText()).equals((botoes[0].getText())))
+                    || ((botoes[2].getText()).equals((botoes[5].getText())) && (botoes[2].getText()).equals((botoes[8].getText())))
+                    || ((botoes[2].getText()).equals((botoes[4].getText())) && (botoes[2].getText()).equals((botoes[6].getText())))) {
+                    
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            case 3:
+                if(((botoes[3].getText()).equals((botoes[0].getText())) && (botoes[3].getText()).equals((botoes[6].getText())))
+                    || ((botoes[3].getText()).equals((botoes[4].getText())) && (botoes[3].getText()).equals((botoes[5].getText())))) {
+                        
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            case 4:
+                if(((botoes[4].getText()).equals((botoes[2].getText())) && (botoes[4].getText()).equals((botoes[6].getText())))
+                    || ((botoes[4].getText()).equals((botoes[3].getText())) && (botoes[4].getText()).equals((botoes[5].getText())))
+                    || ((botoes[4].getText()).equals((botoes[0].getText())) && (botoes[4].getText()).equals((botoes[8].getText()))
+                    || ((botoes[4].getText()).equals((botoes[1].getText())) && (botoes[4].getText()).equals((botoes[7].getText()))))) {
+                        
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            case 5:
+                if(((botoes[5].getText()).equals((botoes[2].getText())) && (botoes[5].getText()).equals((botoes[8].getText())))
+                    || ((botoes[5].getText()).equals((botoes[3].getText())) && (botoes[5].getText()).equals((botoes[4].getText())))) {
+                    
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            case 6:
+                if(((botoes[6].getText()).equals((botoes[3].getText())) && (botoes[6].getText()).equals((botoes[0].getText())))
+                    || ((botoes[6].getText()).equals((botoes[7].getText())) && (botoes[6].getText()).equals((botoes[8].getText())))
+                    || ((botoes[6].getText()).equals((botoes[4].getText())) && (botoes[6].getText()).equals((botoes[2].getText())))) {
+                    
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            case 7:
+                if(((botoes[7].getText()).equals((botoes[1].getText())) && (botoes[7].getText()).equals((botoes[4].getText())))
+                    || ((botoes[7].getText()).equals((botoes[6].getText())) && (botoes[7].getText()).equals((botoes[8].getText())))) {
+                    
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+                
+            case 8:
+                if(((botoes[8].getText()).equals((botoes[0].getText())) && (botoes[8].getText()).equals((botoes[4].getText())))
+                    || ((botoes[8].getText()).equals((botoes[7].getText())) && (botoes[8].getText()).equals((botoes[6].getText())))
+                    || ((botoes[8].getText()).equals((botoes[5].getText())) && (botoes[8].getText()).equals((botoes[2].getText())))) {
+                    
+                    Placar();
+                    return true;
+                } else {
+                    return false;
+                }
+            
+            default:
+                return false;
+        }
+    }
+
+    public void Reset() {
+        vezJogador = 0;
+
+        for(int i = 0; i < 9; i++) {
+            botoes[i].setText("");
+            botoes[i].setEnabled(true);
+        }
+    }
+
+    public void Placar() {
+        if(vezJogador == 0) {
+            jogador1++;
+        } else {
+            jogador2++;
+        }
     }
 }
--- Jogo_da_Velha/src/Jogo_da_Velha.java
+++ Jogo_da_Velha/src/Jogo_da_Velha.java
@@ -1,11 +1,8 @@
 import javax.swing.JFrame;
 
 public class Jogo_da_Velha extends JFrame {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
 
+    private static final long serialVersionUID = 1L;
     public static void main(String[] args) throws Exception {
         Jogo jogo = new Jogo();
         jogo.setDefaultCloseOperation(EXIT_ON_CLOSE); //Determinar que a aplicação seja encerrada após o fechamento da janela;
